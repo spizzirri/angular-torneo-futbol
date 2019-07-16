f744554104b1efbeecb2166c5fd17a13d00b4888
dspizzirri-meli <damian.spizzirri@grupoesfera.com.ar>
2019-07-16 18:08:37 -0300

    Agrega pruebas en servicios

--- src/app/services/equipo.service.spec.ts
+++ src/app/services/equipo.service.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed} from '@angular/core/testing';
 import { EquipoService } from './equipo.service';
 import { HttpClientTestingModule} from '@angular/common/http/testing';
+import { Posicion } from '../models/posicion';
+import { Equipo } from '../models/equipo';
+import { ESCUDOS } from './mocks/equipos/escudos';
 
-describe('DetalleEquipoService', () => {
+fdescribe('DetalleEquipoService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ],
+    imports: [ HttpClientTestingModule ]
   }));
 
   it('should be created', () => {
@@ -12,8 +15,15 @@ describe('DetalleEquipoService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('Debe cargar el detalle de un equipo seleccionado', ()=>{
+  it('Debe cargar el detalle del equipo River Plate', ()=>{
 
     const service: EquipoService = TestBed.get(EquipoService);
+    const posicionSeleccionada:Posicion = { "nombre": "River Plate", "pj": "25", "pg": "13", "pe": "6", "pp": "6", "dg": "", "pts": "45", "cod": "riv" };
+    const detalleDelEquipo:Equipo = { "nombre": "River Plate", "estadio": "Monumental", "apodo": "Millonario", "fundacion": "1901", "escudo": ESCUDOS["ARG"]["riv"].base64 };
+    service.getDetalle(posicionSeleccionada, "ARG");
+
+    service.equipoActual
+      .subscribe((data)=> expect(data).toEqual(detalleDelEquipo));
+
   });
 });
